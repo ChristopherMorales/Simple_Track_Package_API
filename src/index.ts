fae3ed18d6f431912d0d733e6853c7ebeaca67d2
Christopher Morales <christopher.morales4752@gmail.com>
2023-01-02 22:20:26 -0400

    fixed bug of undefined port

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,18 @@
 import * as dotenv from "dotenv";
+dotenv.config();
+
 import config from './config'
 import app from './server'
 
-dotenv.config();
 
 app.listen(config.port, () => {
-    console.log(`Server listening on port ${process.env.PORT}`);
+    console.log(`Server listening on port ${config.port}`);
 })
 
 app.get("/", (req,res) => {
     res.json({message: 'ok'})
 });
+
+process.on('uncaughtException', function (err) {
+    console.log(err);
+}); 
