ea8ca0e3d24180b8346f7ca3754f4acc49d56486
Christopher Morales <christopher.morales4752@gmail.com>
2023-01-03 20:36:25 -0400

    Added Cors to API

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,15 @@
 import * as dotenv from "dotenv";
 dotenv.config();
 
+const cors = require('cors');
+
 import config from './config'
 import app from './server'
 
+app.use(cors({
+    origin: '*',
+    methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH']
+}));
 
 app.listen(config.port, () => {
     console.log(`Server listening on port ${config.port}`);
